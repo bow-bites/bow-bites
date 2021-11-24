5b5d474011411fc67f1a05124cd929a172e99a17
hbzxc <37467710+hbzxc@users.noreply.github.com>
2021-11-23 22:22:23 -1000

    removed only tag on test

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -60,7 +60,7 @@ test('Test the List Vendor page', async (testController) => {
   await listVendorPage.hasMenuItem(testController);
 });
 
-test.only('Test the Add Vendor page', async (testController) => {
+test('Test the Add Vendor page', async (testController) => {
   await navBar.gotoSigninPage(testController);
   await signinPage.signin(testController, credentials.username, credentials.password);
   await navBar.gotoListVendorPage(testController);
