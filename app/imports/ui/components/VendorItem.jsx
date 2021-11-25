cd089258d3c05c184191cc53266c5297c79bf8cc
hbzxc <37467710+hbzxc@users.noreply.github.com>
2021-11-25 03:38:00 -1000

    added favorite button

--- app/imports/ui/components/VendorItem.jsx
+++ app/imports/ui/components/VendorItem.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Container, Item, Button, Popup } from 'semantic-ui-react';
+import { Container, Item, Button, Confirm, Radio } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { Link, withRouter } from 'react-router-dom';
 import { Vendors } from '../../api/vendor/Vendor';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class VendorItem extends React.Component {
+
+  state = { open: false };
+
+  open = () => this.setState({ open: true })
+
+  close = () => this.setState({ open: false })
+
+  test = () => console.log("it CLicked")
 
   deleteVendor = () => {
     Vendors.collection.remove(this.props.vendor._id);
@@ -34,14 +42,15 @@ class VendorItem extends React.Component {
                 <Item.Extra>
                   Link to {this.props.vendor.name}&apos;s Profile page.
                 </Item.Extra>
+                <Item.Extra>
+                  <Button color='green' onClick={this.test}> Favorite </Button>
+                </Item.Extra>
                 <Button as={Link} to={`/edit/${this.props.vendor._id}`} > Edit </Button>
-                <Popup
-                  trigger={
-                    <Button color='red' icon='trash alternate' id="listVendor-Delete" content={delVenTxt} />
-                  }
-                  content={<Button color='green' onClick={this.deleteVendor} id="listVendor-ConfirmDelete">Confirm to Delete {this.props.vendor.name}</Button>}
-                  on='click'
-                  position='top right'
+                <Button color='red' id="listVendor-Delete" onClick={this.open}>{delVenTxt}</Button>
+                <Confirm
+                  open={this.state.open}
+                  onCancel={this.close}
+                  onConfirm={this.deleteVendor}
                 />
               </Item.Content>
             </Item>
