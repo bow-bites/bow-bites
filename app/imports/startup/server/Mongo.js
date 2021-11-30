6c1b98f574901a2097676d17b343baa395febb9b
hbzxc <37467710+hbzxc@users.noreply.github.com>
2021-11-30 11:52:21 -1000

    updated default data

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Vendors } from '../../api/vendor/Vendor';
-// import { Favorites } from '../../api/favorite/Favorite';
+import { Favorites } from '../../api/favorite/Favorite';
 
 /* eslint-disable no-console */
 
@@ -9,24 +9,23 @@ function addVendor(data) {
   console.log(`  Adding: ${data.name} (${data.owner})`);
   Vendors.collection.insert(data);
 }
-/*
+
 function addFavorite(data) {
   console.log(`  Adding: ${data.userId} (${data.liked})`);
   Favorites.collection.insert(data);
 }
-*/
 
 // Initialize the VendrosCollection if empty.
 if (Vendors.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
+  if (Meteor.settings.defaultVendor) {
     console.log('Creating default Vendors.');
     Meteor.settings.defaultVendor.map(data => addVendor(data));
   }
 }
 
-/* if (Favorites.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
+if (Favorites.collection.find().count() === 0) {
+  if (Meteor.settings.defaultFavorites) {
     console.log('Creating default Favorites.');
     Meteor.settings.defaultFavorites.map(data => addFavorite(data));
   }
-} */
+}
