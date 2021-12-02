bfe51664105de5917783ce251bb1b2bcd953728e
coleamparo <camparo@hawaii.edu>
2021-12-02 11:23:33 -1000

    Make a public vendors page, w/o the delete, favorite, and fix signup page

---
+++ app/imports/ui/components/PublicVendorItem.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Container, Item } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class PublicVendorItem extends React.Component {
+  render() {
+    return (
+      <div className="middle-background">
+        <Container>
+          <Item.Group divided>
+            <Item>
+              <Item.Image size='medium' src={this.props.vendor.storeImage}/>
+              <Item.Content verticalAlign="middle">
+                <Item.Header as="h1" id='listVendor-Name'>{this.props.vendor.name}</Item.Header>
+                <Item.Extra>
+                  {this.props.vendor.name} sells {this.props.vendor.foodType} food
+                </Item.Extra>
+                <Item.Description>
+                  {this.props.vendor.description}
+                </Item.Description>
+                <Item.Description>
+                  Open from {this.props.vendor.open} am to {this.props.vendor.close} pm.
+                </Item.Description>
+                <Item.Extra>
+                  Link to {this.props.vendor.name}&apos;s Profile page.
+                </Item.Extra>
+              </Item.Content>
+            </Item>
+          </Item.Group>
+        </Container>
+      </div>
+    );
+  }
+}
+
+// Require a document to be passed to this component.
+PublicVendorItem.propTypes = {
+  vendor: PropTypes.shape({
+    name: PropTypes.string,
+    _id: PropTypes.string,
+    description: PropTypes.string,
+    storeImage: PropTypes.string,
+    foodType: PropTypes.string,
+    open: PropTypes.number,
+    close: PropTypes.number,
+    menuItem: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string,
+      description: PropTypes.string,
+      image: PropTypes.string,
+    })),
+  }).isRequired,
+};
+
+// Wrap this component in withRouter since we use the <Link> React Router element.
+export default withRouter(PublicVendorItem);
---
+++ app/imports/ui/pages/PublicListVendor.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { Container, Item, Header, Loader } from 'semantic-ui-react';
+import { withTracker } from 'meteor/react-meteor-data';
+import PropTypes from 'prop-types';
+import { Vendors } from '../../api/vendor/Vendor';
+import PublicVendorItem from '../components/PublicVendorItem';
+
+/** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
+class ListVendor extends React.Component {
+
+  // If the subscription(s) have been received, render the page, otherwise show a loading icon.
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Getting data</Loader>;
+  }
+
+  // Render the page once subscriptions have been received.
+  renderPage() {
+    return (
+      <Container id="list-vendor-page">
+        <Header as="h2" textAlign="center" inverted>List of Vendors</Header>
+        <Item.Group divided>
+          {this.props.vendors.map((vendor, index) => <PublicVendorItem
+            key={index} vendor={vendor}/>)}
+        </Item.Group>
+      </Container>
+    );
+  }
+}
+
+// Require an array of Stuff documents in the props.
+ListVendor.propTypes = {
+  vendors: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
+
+// withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker
+export default withTracker(() => {
+  // Get access to Stuff documents.
+  const subscription = Meteor.subscribe(Vendors.userPublicationName);
+  // Determine if the subscription is ready
+  const ready = subscription.ready();
+  // Get the Stuff documents
+  const vendors = Vendors.collection.find({}).fetch();
+  return {
+    vendors,
+    ready,
+  };
+})(ListVendor);
