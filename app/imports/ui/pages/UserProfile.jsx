0bba091acc382ad2e605c57a9b035a84bff3de8d
hbzxc <37467710+hbzxc@users.noreply.github.com>
2021-12-07 14:17:50 -1000

    Squashed commit of the following:
    commit 1b7936ae394a7fbde140a24cae64cf838e98ad4a
    Author: hbzxc <37467710+hbzxc@users.noreply.github.com>
    Date:   Tue Dec 7 14:17:23 2021 -1000
    
        reset testcafe
    
    commit fda13441b60551acd9d28b4762df8f0297d29451
    Author: hbzxc <37467710+hbzxc@users.noreply.github.com>
    Date:   Tue Dec 7 14:04:09 2021 -1000
    
        Filter working
    
        Brandon helped me get the dropdown to update the mapping
    
    commit 8a827e04f9285372ff819db7c22e4a72df528cdd
    Author: hbzxc <37467710+hbzxc@users.noreply.github.com>
    Date:   Mon Dec 6 01:53:56 2021 -1000
    
        Update UserProfile.jsx
    
    commit f931fb4b350eca9d6a9ce803780bef44d6af2543
    Author: hbzxc <37467710+hbzxc@users.noreply.github.com>
    Date:   Mon Dec 6 01:09:04 2021 -1000
    
        filter working working on getting it to render
    
        Filter applies correct peram but does not render again

--- app/imports/ui/pages/UserProfile.jsx
+++ app/imports/ui/pages/UserProfile.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Container, Item, Header, Loader } from 'semantic-ui-react';
+import { Container, Item, Header, Loader, Grid } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Vendors } from '../../api/vendor/Vendor';
 import { Favorites } from '../../api/favorite/Favorite';
 import VendorItemUserProfile from '../components/VendorItemUserProfile';
 import Anything from '../components/Anything';
+import TypeFilter from '../components/TypeFilter';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class UserProfile extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { newFilteredArr: [] };
+  }
+
+  filterVendor = (newArr, userPro, data) => {
+    // Type filter
+    const newFilteredArr = [];
+    const sampleArr = [];
+    if (data.filtered) {
+      data.filtered.forEach(fType => sampleArr.push(fType));
+    }
+    sampleArr.forEach(typeObj => {
+      const filterUser = newArr.filter(type => type.foodType === typeObj);
+      if (sampleArr) {
+        filterUser.forEach(element => newFilteredArr.push(element));
+      }
+    });
+    if (sampleArr.length < 1 && userPro) {
+      userPro.liked.forEach(element => newFilteredArr.push(Vendors.collection.find({ _id: element.favorite }).fetch()[0]));
+    }
+    return newFilteredArr;
+  }
 
   // If the subscription(s) have been received, render the page, otherwise show a loading icon.
   render() {
@@ -22,22 +47,42 @@ class UserProfile extends React.Component {
     const userName = userEmail.split('@');
     const pageName = `${userName[0]}'s Page`;
     const user = Meteor.user().username;
+
+    // connect the liked profile to the user
     const userPro = Favorites.collection.find({ userId: user }).fetch()[0];
     const newArr = [];
     if (userPro) {
       userPro.liked.forEach(element => newArr.push(Vendors.collection.find({ _id: element.favorite }).fetch()[0]));
     } else {
-      console.log('userPro empty');
+      // console.log('userPro empty');
     }
+    let newFilteredArr = this.filterVendor(newArr, userPro, []);
+
+    const eventhandler = data => {
+      newFilteredArr = [];
+      this.filterVendor(newArr, userPro, data).forEach(foodType => newFilteredArr.push(foodType));
+      this.setState({ newFilteredArr: newFilteredArr });
+    };
 
     return (
       <Container id="user-profile-page">
         <Header as="h2" textAlign="center" inverted>{pageName}</Header>
-        <Item.Group divided>
-          <Anything/>
-          {newArr.map((vendor, index) => <VendorItemUserProfile
-            key={index} vendor={vendor}/>)}
-        </Item.Group>
+        <Grid divided='vertically'>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Item.Group divided>
+                <Anything/>
+                {newArr.map((vendor, index) => <VendorItemUserProfile
+                  key={index} vendor={vendor} onUpdate={eventhandler}/>)}
+              </Item.Group>
+            </Grid.Column>
+            <Grid.Column>
+              <TypeFilter onChange={eventhandler}/>
+              {this.state.newFilteredArr.map((vendor, index) => <VendorItemUserProfile
+                key={index} vendor={vendor} />)}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Container>
     );
   }
