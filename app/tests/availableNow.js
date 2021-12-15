bc28514b2107cc140f31d59cb2435c652f8a6422
coleamparo <camparo@hawaii.edu>
2021-12-14 18:12:55 -1000

    remove waiting time for AvailableNow test

--- app/tests/availableNow.js
+++ app/tests/availableNow.js
@@ -9,7 +9,7 @@ class AvailableNowPage {
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
     // This is first test to be run. Wait 120 seconds to avoid timeouts with GitHub Actions.
-    await testController.wait(120000).expect(this.pageSelector.exists).ok();
+    await testController.expect(this.pageSelector.exists).ok();
   }
 
   /** Makes sure AvailableNow has Menu Items displayed */
